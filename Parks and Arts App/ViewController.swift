28462f61112f8ce1de5bffb3076a07458af1b645
Kyle Kamibayashi <decokyle@hotmail.com>
2018-05-13 22:05:01 -0700

    Added public parks data into json file. Added annotations to parks.

--- Parks and Arts App/ViewController.swift
+++ Parks and Arts App/ViewController.swift
@@ -1,25 +1,55 @@
-//
-//  ViewController.swift
-//  Parks and Arts App
-//
-//  Created by Kyle Kamibayashi on 5/13/18.
-//  Copyright © 2018 Kyle Kamibayashi. All rights reserved.
-//
-
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        
+        let initialLocation = CLLocation(latitude: 37.74140787, longitude: -122.43319872)
+        
+        let regionRadius: CLLocationDistance = 5000
+        func centerMapOnLocation(location: CLLocation){
+            let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+            mapView.setRegion(coordinateRegion, animated: true)
+        }
+        
+        centerMapOnLocation(location: initialLocation)
+        
+        let park = Parks(title: "Billy Goat Hill", parkType: "Neighboorhood park or playground", coordinate: CLLocationCoordinate2D(latitude:37.74140787 , longitude: -122.43319872))
+        mapView.addAnnotation(park)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
 }
-
+extension ViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Parks
+            else{return nil}
+        
+        let identifier = "marker"
+        var view: MKMarkerAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        }else{
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
+    }
+}
